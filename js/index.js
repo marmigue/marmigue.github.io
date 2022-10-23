160b38adaa9bd36670dd72a1c8bed1918eccbd3c
Marcelo Colman <marmigue96@gmail.com>
2022-10-23 18:39:33 -0300

    Carga de documentos

--- js/index.js
+++ js/index.js
@@ -1285,3 +1285,89 @@ const changeTab = (numVentana, e)=>{
 ventanas[0].addEventListener( 'click', (e)=> changeTab( 0, e) );
 ventanas[1].addEventListener( 'click', (e)=> changeTab( 1, e) );
 ventanas[2].addEventListener( 'click', (e)=> changeTab( 2, e) );
+
+
+
+
+/*------------------------------------Drag and Drop---------------------------------------*/
+
+const dropArea = document.querySelector( '.drop-area' );
+const dragText = dropArea.querySelector( 'h2' );
+const button = dropArea.querySelector( 'button' );
+const input = dropArea.querySelector( '#input-file' );
+const preview = document.querySelector( '#preview' );
+let files;
+let arrayFiles = [];
+
+const llamarInput = ()=>{
+  input.click();
+}
+
+
+button.addEventListener( "click", llamarInput );
+input.addEventListener( "change", (e)=>{
+  files = e.target.files;
+  dropArea.classList.add("active");
+  showFiles(files);
+  dropArea.classList.remove('active');
+} );
+
+
+dropArea.addEventListener( "dragover", (e)=>{
+  e.preventDefault();
+  dropArea.classList.add( "active" );
+  dragText.textContent = 'Suelta para subir los archivos';
+} );
+
+dropArea.addEventListener( "dragleave", (e)=>{
+  e.preventDefault();
+  dropArea.classList.remove( "active" );
+  dragText.textContent = 'Arrastra y suelta los documentos';
+} );
+
+dropArea.addEventListener( "drop", (e)=>{
+  e.preventDefault();
+  files = e.dataTransfer.files;
+  showFiles(files);
+  dropArea.classList.remove( "active" );
+  dragText.textContent = 'Arrastra y suelta los documentos';
+} );
+
+
+function showFiles(files){
+  if(files.length=== undefined){
+    processFile(files);
+    arrayFiles.push(files);
+  }else{
+    for(const file of files){
+      processFile(file);
+      arrayFiles.push(file);
+    }
+  }
+}
+
+
+function processFile(file){
+  const docType = file.type;
+  const validExtensions = ['image/jpeg', 'image/jpg', 'image/png', 'text/txt'];
+  if(validExtensions.includes(docType)){
+    const fileReader = new FileReader();
+    const id = `file-${Math.random().toString(32).substring(7)}`;
+    fileReader.addEventListener( 'load', e=>{
+      const fileUrl = fileReader.result;
+      const image = `
+        <div id=${id} class="card">
+          <img src="${fileUrl}" alt="${file.name}" width="50px">
+          <span>${file.name}</span>
+        </div>
+      `;
+      let html = preview.innerHTML;
+      preview.innerHTML = html + image;
+
+    } );
+    fileReader.readAsDataURL(file);
+    // uploadFile(file, id)
+  }else{
+    alert('Error');
+  }
+}
