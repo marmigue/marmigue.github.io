485b1aa2ff8aefc3a0ae24b0346b8dfd1e33e699
Marcelo Colman <marmigue96@gmail.com>
2023-02-12 20:12:57 -0300

    Correcion de error

--- js/index.js
+++ js/index.js
@@ -342,26 +342,26 @@ const infoConfirm = ()=>{
   else
     infoSection[35].style.display = 'none';
   if(arrayFiles[2] !== false){
-    infoDoc[0].innerText = 'prueba3';
+    infoDoc[2].innerText = 'prueba3';
     infoSection[36].style.display = 'block';
   }
   else
     infoSection[36].style.display = 'none';
   if(arrayFiles[3] !== false){
-    infoDoc[1].innerText = 'prueba4';
+    infoDoc[3].innerText = 'prueba4';
     infoSection[37].style.display = 'block';
   }
   else
     infoSection[37].style.display = 'none';
   if(arrayFiles[4] !== false){
-    infoDoc[1].innerText = 'prueba4';
+    infoDoc[4].innerText = 'prueba4';
     infoSection[38].style.display = 'block';
   }
   else
     infoSection[38].style.display = 'none';
   if(arrayFiles[5] !== false){
+    infoDoc[5].innerText = 'prueba4';
     infoSection[39].style.display = 'block';
-    infoDoc[1].innerText = 'prueba4';
   }
   else
     infoSection[39].style.display = 'none';
